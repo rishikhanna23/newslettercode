5387d3f5fae50849263ee905f75f012fc6481660
rishikhanna23 <rishi.khanna.23@gmail.com>
2021-09-13 09:37:55 +0530

    variable port

--- app.js
+++ app.js
@@ -100,8 +100,8 @@ res.redirect( "/");
 //'{"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"double_optin":false,"marketing_permissions":false}'
 //Create javascript object then convert to JSON
 
-
-app.listen(3000,function()
+const port =process.env.port ||3000;
+app.listen(port,()=>
   {
 
 console.log("server is running on port 3000");
